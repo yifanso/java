407178f500b33243e95637a48d5981a38a89cad5
Yifan Song (yifsong) <yifsong@cisco.com>
2019-11-07 13:50:19 +0800

    init

---
+++ src/main/java/com/yun/song/immediate/ImmediateApplication.java
@@ -0,0 +1,13 @@
+package com.yun.song.immediate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImmediateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImmediateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yun/song/immediate/ImmediateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yun.song.immediate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImmediateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
